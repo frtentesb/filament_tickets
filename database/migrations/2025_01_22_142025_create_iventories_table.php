5cd0a16758ae1788375cba0651abe9ac5a086bcb
frtentesb <frtentesb@gmail.com>
2025-01-22 22:05:28 -0300

    Correção das foreignIdFor iventory e Iventory_transactions nas migrates.

--- database/migrations/2025_01_22_142025_create_iventories_table.php
+++ database/migrations/2025_01_22_142025_create_iventories_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,7 @@ return new class extends Migration
     {
         Schema::create('iventories', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('product_id')->primary();
-            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete();
             $table->string('type');
             $table->integer('quantity')->default(0);
             $table->decimal('unit_price');
--- database/migrations/2025_01_22_142106_create_iventory_transactions_table.php
+++ database/migrations/2025_01_22_142106_create_iventory_transactions_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\Product;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,8 +14,7 @@ return new class extends Migration
     {
         Schema::create('iventory_transactions', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('product_id')->primary();
-            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreignIdFor(Product::class)->constrained()->cascadeOnDelete();
             $table->string('category');
             $table->string('manufacturer');
             $table->double('sale_price');
