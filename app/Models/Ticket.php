a1abd2780370234a3e4cf4fd6795d83e8cc51dc2
wallace martins <wallacemartinss@gmail.com>
2025-01-24 13:41:40 -0300

    Alteração na Model de TIcket - Implantação funcionalidade para usuario so ver os tickets q ele abriu!

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Facades\Auth;
 use App\Enums\Tickets\StatusTicketEnum;
 use Illuminate\Database\Eloquent\Model;
 use App\Enums\Tickets\CategoryTicketEnum;
 use App\Enums\Tickets\PriorityTicketEnum;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -34,6 +36,25 @@ protected $casts = [
 
 ];
 
+
+//Função para permitir que o Usuarui logado veja apenas os seus tickets
+//Administrador poderá ver todos os tickes tanto no painel App quanto no painel Admin
+protected static function booted()
+    {
+        static::addGlobalScope('user_tickets', function (Builder $builder) {
+            // Aplica a lógica apenas para usuários não administradores
+            if (Auth::check() && !Auth::user()->is_admin) {
+                if ($builder instanceof \Illuminate\Database\Eloquent\Builder) {
+                    $builder->where('user_id', Auth::user()->id);
+                } else {
+                    // Trata o caso em que $builder não é uma instância de Builder
+                    // Por exemplo, você pode lançar uma exceção ou registrar um erro
+                    throw new \Exception('Instância de construtor inválida');
+                }
+            }
+        });
+    }
+
 public function user(): BelongsTo
 {
     return $this->belongsTo(User::class);
@@ -45,5 +66,5 @@ public function ticketresponses(): HasMany
     return $this->hasMany(TicketResponse::class, 'ticket_id');
 }
 
-    //
+
 }
